c85dd1213252c1dcaa59ab92cee82cc8f9d6711c
WM-Jo97 <chohj5509@gmail.com>
2022-07-25 23:53:15 +0900

    today

--- exemple.py
+++ exemple.py
@@ -1,7 +1,10 @@
-T=int(input())
+def leap_year(year):
+    if year%4==0 and year%100!=0:
+        return f'{year}년은 윤년이입니다.'
+    elif year%400 == 0:
+        return f'{year}년은 윤년이입니다.'
+    else:
+        return f'{year}년은 윤년이 아닙니다.'
 
-Day=int(input())
-Price=[]
-Price.append(input().split())
-
-print (Price)
+print(leap_year(2021))
+print(leap_year(2020))
