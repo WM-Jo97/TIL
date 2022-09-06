34e34c4eaa091454157695d92932dafe7f70c30a
WM-Jo97 <chohj5509@gmail.com>
2022-09-07 00:14:13 +0900

    today

--- ALGORITHM/0906/SWEA_1952.py
+++ ALGORITHM/0906/SWEA_1952.py
@@ -10,4 +10,33 @@ for t in range(1,T+1):
     print(charge_list)
     print(day_year)
 
-    for i in range(len(charge_list)):
+    check_month = charge_list[1]//charge_list[0]
+    check_3month = charge_list[2]//(3*charge_list[1])
+    check_year_month = charge_list[3]/(12*charge_list[1])
+    check_year_3mon = charge_list[3]/(4*charge_list[2])
+
+    #print(check_month)
+    #print(check_3month)
+    #print(check_year_month)
+    #print(check_year_3mon)
+
+    check_month_list = [[0] for _ in range(12)]
+    for i in range(len(day_year)):
+        if charge_list[1] <= day_year[i]*charge_list[0]:
+            check_month_list[i] = charge_list[1]
+        else:
+            check_month_list[i] = charge_list[0]*day_year[i]
+
+    print(check_month_list)
+
+    check_3month_list = [[0] for _ in range(12)]
+
+    for j in range(len(day_year)-2):
+        if charge_list[2] <= check_month_list[j]+check_month_list[j+1]+check_month_list[j+2]:
+            check_3month_list[j] = charge_list[2]
+            check_month_list[j+1] = 3000
+            check_month_list[j+2] = 3000
+        else:
+            check_3month_list[j] = check_month_list[j]
+
+    print(check_3month_list)
