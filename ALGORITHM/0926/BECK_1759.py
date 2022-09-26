c9e67ceaa3237101c01a9b96b40638ce6b6dc17d
WM-Jo97 <chohj5509@gmail.com>
2022-09-26 22:44:16 +0900

    today

--- ALGORITHM/0926/BECK_1759.py
+++ ALGORITHM/0926/BECK_1759.py
@@ -8,11 +8,30 @@ alpa = list(input().split())
 
 a_list = ['a','e','i','o','u']
 
-print(alpa)
-
 password = list(combinations(alpa,L))
+
+PASS_LIST = []
+
+PASSWORD_LIST = []
 for i in range(len(password)):
-    if password[i] in
+    TEMP = []
+    for j in range(len(password[i])):
+        TEMP.append(password[i][j])
+    PASSWORD_LIST.append(TEMP)
 
+for i in range(len(PASSWORD_LIST)):
+    count = 0
+    word = ''
+    PASSWORD_LIST[i].sort()
 
-print(password)
+    for j in range(len(PASSWORD_LIST[i])):
+        word += PASSWORD_LIST[i][j]
+        if PASSWORD_LIST[i][j] in a_list:
+            count += 1
+
+    if 1<= count <L-1:
+        PASS_LIST.append(word)
+
+PASS_LIST.sort()
+for i in PASS_LIST:
+    print(i)
