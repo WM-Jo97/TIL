c275156ed70281c91257e9908e8a453625230235
WM-Jo97 <chohj5509@gmail.com>
2022-11-08 23:35:55 +0900

    today

--- Study/11_9/Q11_Snake.py
+++ Study/11_9/Q11_Snake.py
@@ -15,19 +15,19 @@ for _ in range(Change):
     Change_list.append((int(X),C))
 
 NUM = 0
-location = (0,0)
-second = 0
+second = 1
 move = (0,1)
 print(Change_list)
 print(arr)
-
+snake = [(0,1)]
+Num_cg, Chg = Change_list.pop(0)
 while True:
-    Num_cg, Chg = Change_list[NUM]
-    y,x = location
-    y_o, x_o = move
-    if arr[y][x] != 1:
-        arr[y][x] = 1
+    y,x = snake[-1]
+    print(snake[-1])
+
+    if 0<=y<N and 0<=x<N or (y,x) not in snake:
         if second == Num_cg:
+            print(Chg)
             if Chg == 'D':
                 if move == (0,1):
                     move = (1,0)
@@ -46,15 +46,34 @@ while True:
                     move = (1,0)
                 elif move == (1,0):
                     move = (0,1)
-            if arr[y][x] == 2:
-                pass
-            else:
-                pass
-        else:
+
+            if Change_list:
+                Num_cg,Chg = Change_list.pop(0)
+
+
+            y_o, x_o = move
             location = (y+y_o, x+x_o)
+
             if arr[y][x] == 2:
-                pass
+                snake.append(location)
+                second += 1
             else:
-                pass
+                snake.pop(0)
+                snake.append(location)
+                second += 1
+
+        else:
+            y_o, x_o = move
+            location = (y + y_o, x + x_o)
+            if arr[y][x] == 2:
+                snake.append(location)
+                second += 1
+            else:
+                snake.pop(0)
+                snake.append(location)
+                second += 1
     else:
+        print(snake)
+        print(second)
+        print(-1)
         break
